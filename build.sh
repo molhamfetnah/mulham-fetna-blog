d119d885bff78501ffc22639b4bedf63224e33a9
Mulham Fetna <molhamfetneh@gmail.com>
2026-03-29 20:36:06 +0300

    Clean for Cloudflare Pages

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "🚀 Using Cloudflare's built-in Hugo..."
-git submodule update --init --recursive
-hugo --gc --minify
-echo "✨ Built $(find public -name '*.html' | wc -l) pages"
-ls -la public/
